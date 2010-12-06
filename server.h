7e84cea77d37c8ab6384f71a969a74056687ca9b
fakessh @ and gabriel project <fakessh@fakessh.eu>
2010-12-06 17:59:57 +0100

    0.0.1 commit

--- server.h
+++ server.h
@@ -4,7 +4,7 @@
 #define ServerKEYFILE "mycert.pem"
 #define ServerPASSWORD ""
 #define DHFILE "dh1024.pem"
-
+SSL_CTX* InitServerCTX(void);
 int tcp_listen(void);
 void load_dh_params(SSL_CTX *ctx,char *file);
 void generate_eph_rsa_key(SSL_CTX *ctx);
