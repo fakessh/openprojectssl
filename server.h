3570d512d3ca0ba1530a88acb229b90fe89f7ebf
fakessh @ and gabriel project <fakessh@fakessh.eu>
2010-12-06 18:35:31 +0100

    0.0.1 commit

--- server.h
+++ server.h
@@ -5,6 +5,8 @@
 #define ServerPASSWORD ""
 #define DHFILE "dh1024.pem"
 SSL_CTX* InitServerCTX(void);
+void LoadCertificates(SSL_CTX* ctx, char* CertFile, char* KeyFile);
+void Servlet(SSL* ssl);
 int tcp_listen(void);
 void load_dh_params(SSL_CTX *ctx,char *file);
 void generate_eph_rsa_key(SSL_CTX *ctx);
