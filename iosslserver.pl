af6cdb805ac3277166dd88094c21ace1e7c7c49b
fakessh @ <serge@smtp.fakessh.eu>
2012-04-06 05:05:11 +0200

    0.0.7

--- iosslserver.pl
+++ iosslserver.pl
@@ -5,15 +5,27 @@
  use Net::hostent;              # pour la version OO de gethostbyaddr
  use strict;
  use warnings;
-my  $PORT = 42000;                 
+my  $PORT = 42000;
+
+$IO::Socket::SSL::DEBUG = 3;
+# Check to make sure that we were not accidentally run in the wrong
+
+# directory:
+
+unless (-d "certs") {
+    if (-d "./certs") {	    
+	   chdir "./certs";	    
+       } else {		    
+	  die "chdir impossible!\n";		    
+      }
+}
 
 my $server = IO::Socket::SSL->new( Proto     => 'tcp',
                                   LocalPort => $PORT,
                                   Listen    => 5 ,
                                   SSL_verify_mode => 0x04,
-                                  SSL_key_file    => '/etc/pki/tls/private/ks37777.kimsufi.com.key',
-                                  SSL_cert_file   => '/etc/pki/tls/certs/ks37777.kimsufi.com.cert',
-                                  Reuse     => 1);
+                                  Reuse     => 1,
+                                  SSL_passwd_cb => sub {return "bluebell"});
 
  die "can't setup server" unless $server;
  print "[Server $0 accepting clients]\n";
