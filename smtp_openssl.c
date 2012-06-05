f791b5a74c88813137705b92657742a8fdb7533b
fakessh @ <serge@smtp.fakessh.eu>
2012-06-06 00:04:53 +0200

    0.0.7

--- smtp_openssl.c
+++ smtp_openssl.c
@@ -32,7 +32,7 @@
 #include <linux/types.h>
 #endif
 
-#define   CHK_NULL(x)   if   ((x)==NULL)   exit   (1)
+#define   CHK_NULL(x)   do { if   ((x)==NULL)   exit   (1); } while(0)
 #define   CHK_ERR(err,s)   if   ((err)==-1)   {   perror(s);   exit(1);   }
 #define   CHK_SSL(err)   if   ((err)==-1)   {   ERR_print_errors_fp(stderr);   exit(2);   }
 
